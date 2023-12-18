1a0b4d511c17354e7ee92d0987af9d084d6cfa0f
yasasvi <yasasvigumma@gmail.com>
2023-12-18 19:18:30 +0530

    testing api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,25 @@
 <script>
-  fetch("http://127.0.0.1:5000")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  import { onMount } from "svelte";
+
+  let use;
+  onMount(async () => {
+    await fetch("https://dummyjson.com/products/1")
+      .then((res) => res.json())
+      .then((data) => {
+        use = data;
+        console.log(use);
+      });
+  });
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+<div>
+  {#if use !== undefined}
+    {use?.brand}<br />
+    {use?.title}<br />
+    <img src={use?.images[0]} alt={use?.title} />
+  {/if}
+</div>
