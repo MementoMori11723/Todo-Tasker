223d8743ecb5d67299817006d79bb32cc204fbeb
yasasvi <yasasvigumma@gmail.com>
2023-12-18 18:57:01 +0530

    testing api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+  fetch("http://127.0.0.1:5000")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
