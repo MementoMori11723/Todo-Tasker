ffaef91b92f77d796fe0e0f4cd4ee36c286d4ccd
yasasvi <yasasvigumma@gmail.com>
2023-12-23 19:29:26 +0530

    testing a simple ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,47 @@
-<!-- <script>
-  let use;
-  const run = async () =>
-    await fetch("http://127.0.0.1:3000")
+<script>
+  import { onMount } from "svelte";
+  let todos;
+  onMount(() => {
+    fetch("https://www.example.com")
       .then((res) => res.json())
-      .then((data) => {
-        use = data;
-        console.log(use);
+      .then((data) => (todos = data))
+      .catch((err) => {
+        console.error("Unable to fetch the data! " + err);
       });
-  run();
-</script> -->
+  });
+  if (todos === undefined)
+    todos = [
+      { text: "Todo 1", done: false },
+      { text: "Todo 2", done: false },
+    ];
+</script>
 
-<h1 class="text-red-700">Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
-<!-- <div>
-  {#if use !== undefined}
-    {use?.age}<br />
-    {use?.name}<br />
-  {/if}
-</div> -->
+<h1 class="font-sans text-3xl font-semibold">TODO List<br /><br /></h1>
+<div class="todos font-sans text-lg font-normal">
+  {#each todos as todo, i (i)}
+    <div class="todo">
+      <label
+        for="checked"
+        id="check"
+        type="text"
+        class={`${todo.done ? "line-through" : ""}`}>{todo.text}</label
+      >
+      <input
+        type="checkbox"
+        name="checked"
+        checked={todo.done}
+        on:click={(e) => (todo.done = !todo.done)}
+      />
+    </div>
+    <br />
+  {/each}
+</div>
+
+<style>
+  :root {
+    margin-top: 70px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+</style>
