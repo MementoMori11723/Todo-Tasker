f1d4a2eedc93e05108f821c7fae4d63041f4ee2a
yasasvi <yasasvigumma@gmail.com>
2024-06-05 19:58:36 +0530

    using the switch to change text on button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,11 @@
     let loginSwitch = false;
 </script>
 
-<button on:click={ () => loginSwitch = true }>Click me!</button>
+<button on:click={ () => loginSwitch = true }>{loginSwitch ? "Logout" : "Login"}</button>
 
 {#if loginSwitch}
     <h1>Logged in!</h1>
 {/if}
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
