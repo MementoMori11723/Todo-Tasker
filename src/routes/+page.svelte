0d3da62b63ea3370d7b6b89f87adf52764d3ab52
yasasvi <yasasvigumma@gmail.com>
2024-06-05 19:53:56 +0530

    adding a switch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
-<button on:click={() => alert("Button clicked!")}>Click me!</button>
+<script>
+    let loginSwitch = false;
+</script>
+
+<button on:click={() => alert(`Button ${loginSwitch ? 'success' : 'failed'}!`)}>Click me!</button>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
