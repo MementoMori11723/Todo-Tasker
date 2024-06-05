5009e305b62c5a94c13328ca2ec30fb39cbd84f3
yasasvi <yasasvigumma@gmail.com>
2024-06-05 19:56:30 +0530

    using the switch in homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,10 @@
     let loginSwitch = false;
 </script>
 
-<button on:click={() => alert(`Button ${loginSwitch ? 'success' : 'failed'}!`)}>Click me!</button>
+<button on:click={ () => loginSwitch = true }>Click me!</button>
 
+{#if loginSwitch}
+    <h1>Logged in!</h1>
+{/if}
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
