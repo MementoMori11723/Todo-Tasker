c808b47a091a02d58103230ac194d0b9ce841271
yasasvi <yasasvigumma@gmail.com>
2024-06-05 20:16:14 +0530

    adding a link to about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,18 @@
 <script>
-    let loginSwitch = false;
+  let loginSwitch = false;
 </script>
 
-<button on:click={ () => loginSwitch = !loginSwitch }>{loginSwitch ? "Logout" : "Login"}</button>
+<button on:click={() => (loginSwitch = !loginSwitch)}
+  >{loginSwitch ? "Logout" : "Login"}</button
+>
+
+<a href="/about" style="margin: 10px;">About</a>
 
 {#if loginSwitch}
-    <h1>Logged in!</h1>
+  <h1>Logged in!</h1>
 {:else}
-    <h1>Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  <h1>Welcome to SvelteKit</h1>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
 {/if}
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,16 @@
-<h1>About page</h1>
+<h1>About Page</h1>
+<p>
+  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos pariatur numquam
+  similique quod voluptatibus nihil nisi fugiat assumenda, odit sequi
+  consequuntur vitae animi. Laudantium, distinctio corrupti. Facere et sit
+  quaerat. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, eos,
+  minima fugit rerum quo corporis accusantium eaque exercitationem aut
+  dignissimos, nam ducimus soluta natus quibusdam voluptas laboriosam illum
+  debitis dolore? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+  Doloribus aliquam inventore pariatur eaque placeat. Doloribus, laudantium quia
+  quasi ut aliquam voluptatibus ullam, officiis tenetur commodi cum, magnam
+  beatae impedit veritatis. Lorem ipsum dolor sit amet consectetur adipisicing
+  elit. Doloribus aliquam inventore pariatur eaque placeat. Doloribus,
+  laudantium quia quasi ut aliquam voluptatibus ullam, officiis tenetur commodi
+  cum, magnam
+</p>
