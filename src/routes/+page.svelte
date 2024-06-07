1d7feb71f5af37e258e15d838d2e40fb404a7fd0
yasasvi <yasasvigumma@gmail.com>
2024-06-07 14:35:14 +0530

    added a checkbox

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,22 @@
 <script>
   let loginSwitch = false;
   let tasks = [{id:1,name:"task-1"},{id:2,name:"task-2"},{id:3,name:"task-3"}]
-  let saveState = false
 </script>
 
-<button on:click={() => (loginSwitch = !loginSwitch)}
-  >{loginSwitch ? "Logout" : "Login"}</button
->
+<button on:click={() => {
+  loginSwitch = !loginSwitch
+}}>{loginSwitch ? "Logout" : "Login"}</button>
 
 <a href="/about" style="margin: 10px;">About</a>
 
 {#if loginSwitch}
   <h1>Logged in!</h1>
-  <ul>
+  <div>
     {#each tasks as task (task.id)}
-      <li>{task.name}</li>
+      <input type="checkbox" name={`${task.id}`} >
+      <label for={`${task.id}`}>{task.name}</label><br/>
     {/each}
-  </ul>
+  </div>
 {:else}
   <h1>Welcome to SvelteKit</h1>
   <p>
