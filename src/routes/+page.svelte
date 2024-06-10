756abce73d3f4042c8cec1f78b8b6c4e8c76f57f
yasasvi <yasasvigumma@gmail.com>
2024-06-10 22:21:33 +0530

    get api works with load function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,26 +6,7 @@
   export let data: PageData;
   $: console.log(data);
   let loginSwitch = false;
-  let tasks = [
-    {
-      id: 1,
-      name: "task-1",
-      description: "This is task - 1",
-      lineThrough: false,
-    },
-    {
-      id: 2,
-      name: "task-2",
-      description: "This is task - 2",
-      lineThrough: false,
-    },
-    {
-      id: 3,
-      name: "task-3",
-      description: "This is task - 3",
-      lineThrough: false,
-    },
-  ];
+  let tasks = data.tasks;
 
   onMount(() => {
     sessionStorage.getItem("name")
