1b0db64299da80fefa6397fcd1b5708e826e8e7e
yasasvi <yasasvigumma@gmail.com>
2024-06-16 19:49:56 +0530

    added an update function

--- src/lib/database/database.ts
+++ src/lib/database/database.ts
@@ -12,14 +12,28 @@ function connectDb(): Database | object {
 function addData(db: Database, data: object): object {
   try {
     const { id, name, description, status }: any = data;
-    db.prepare("INSERT INTO Tasks VALUES (?,?,?,?)").run(
-      id,
-      name,
-      description,
-      status
-    );
+    db.prepare(
+      "INSERT INTO Tasks(userid, title, description, status) VALUES (?,?,?,?)"
+    ).run(id, name, description, status);
+    db.close();
     return { success: true };
   } catch (err) {
+    db.close();
+    return { success: false, error: err };
+  }
+}
+
+// need to add a function to get data from the database, in case of any object variables are empty or undefined!
+function updateData(db: Database, data: object): object {
+  try {
+    const { id, name, description, status }: any = data;
+    db.prepare(
+      "UPDATE Tasks SET title = ?, description = ?, status = ? WHERE userid = ?"
+    ).run(name, description, status, id);
+    db.close();
+    return { success: true };
+  } catch (err) {
+    db.close();
     return { success: false, error: err };
   }
 }
