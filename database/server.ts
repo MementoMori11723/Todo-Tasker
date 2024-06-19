0e8e177afe7494d0ca0e92ec6685f3ad6895c6b0
yasasvi <yasasvigumma@gmail.com>
2024-06-19 11:33:28 +0530

    adding other http methods to server

--- database/server.ts
+++ database/server.ts
@@ -5,32 +5,70 @@ import { connectDb, getData } from "./database";
 console.log(
   "Server is running on http://localhost:8080\nPress 'q' to stop the server."
 );
+
+function returnHeader(msg: object) {
+  const header = {
+    "content-type": "application/json",
+    "Allow-Access-Control-Origin": "*",
+  };
+  return new Response(JSON.stringify(msg), {
+    headers: header,
+  });
+}
+
+function getResponse(db: any) {
+  const tasks = getData(db, "1");
+  if (!tasks) {
+    return returnHeader({ message: "No tasks found" });
+  }
+  return returnHeader({ tasks });
+}
+
+// need to configure this
+function postResponse(db: any) {
+  const tasks = getData(db, "1");
+  if (!tasks) {
+    return returnHeader({ message: "No tasks found" });
+  }
+  return returnHeader({ tasks });
+}
+
+// need to configure this
+function putResponse(db: any) {
+  const tasks = getData(db, "1");
+  if (!tasks) {
+    return returnHeader({ message: "No tasks found" });
+  }
+  return returnHeader({ tasks });
+}
+
+// need to configure this
+function deleteResponse(db: any) {
+  const tasks = getData(db, "1");
+  if (!tasks) {
+    return returnHeader({ message: "No tasks found" });
+  }
+  return returnHeader({ tasks });
+}
+
 const server = serve({
   fetch: async (req: Request) => {
     const db = await connectDb();
-    let tasks;
-    if (db != undefined) {
-      tasks = getData(db, "1");
-    } else {
-      return new Response("Database connection failed", {
-        headers: {
-          "content-type": "text/plain",
-        },
-      });
+    if (db == undefined) {
+      return returnHeader({ message: "Database not connected" });
     }
-    if (!tasks) {
-      return new Response("No tasks found", {
-        headers: {
-          "content-type": "text/plain",
-        },
-      });
+    switch (req.method) {
+      case "GET":
+        return getResponse(db);
+      case "POST":
+        return postResponse(db);
+      case "PUT":
+        return putResponse(db);
+      case "DELETE":
+        return deleteResponse(db);
+      default:
+        return returnHeader({ message: "Invalid request" });
     }
-    return new Response(JSON.stringify(tasks), {
-      headers: {
-        "content-type": "application/json",
-        "Allow-Access-Control-Origin": "*",
-      },
-    });
   },
   port: 8080,
 });
