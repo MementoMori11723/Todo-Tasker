9a98b83680ecdc492ec880f3b3b4be4e0762a6a8
yasasvi <yasasvigumma@gmail.com>
2024-06-19 11:03:56 +0530

    removing long comments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,36 +19,6 @@
   onMount(() => {
     localStorage.getItem("name") ? (loginSwitch = true) : (loginSwitch = false);
   });
-
-  // const responce = fetch("/api", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({
-  //     id: 4,
-  //     name: "SvelteKit",
-  //     description:
-  //       "SvelteKit is a framework for building web applications of all sizes, with a beautiful development experience and flexible filesystem-based routing.",
-  //   }),
-  // }).then((res) => {
-  //   res.json().then((data) => {
-  //     console.log(data);
-  //   });
-  // });
-
-  // const putResponce = fetch("/api", {
-  //   method: "PUT",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({
-  //     id: 2,
-  //     name: "Title Updated!",
-  //   }),
-  // }).then((res) => {
-  //   res.json().then((data) => console.log(data));
-  // });
 </script>
 
 <svelte:head>
