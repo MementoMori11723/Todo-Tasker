f10e60b905031e25be4cafc21f227aa55a08fad8
yasasvi <yasasvigumma@gmail.com>
2024-10-04 11:55:23 +0530

    using react for frontend
    should be good enough...

--- client/src/lib/index.ts
+++
@@ -1,3 +0,0 @@
-// Home page components
-export { default as Navbar } from "$lib/navbar.svelte";
-export { default as Login } from "$lib/login.svelte";
--- client/src/lib/navbar.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  export let loginSwitch: boolean;
-  export let login: () => void;
-</script>
-
-<button on:click={login}>{loginSwitch ? "Logout" : "Login"}</button>
-<a href="/about" style="margin: 10px;">About</a>
