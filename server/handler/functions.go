72699b51d83321d7ae685eeb2dad215897c38c0d
yasasvi <yasasvigumma@gmail.com>
2024-10-05 20:12:20 +0530

    made another change
    this should work...

--- server/handler/functions.go
+++ server/handler/functions.go
@@ -23,6 +23,9 @@ type User struct {
 var (
 	Routes = map[string]func(http.ResponseWriter, *http.Request) {
     "GET /tasks": GetTask,
+    "POST /tasks": InsertTask,
+    "PUT /tasks": UpdateTask,
+    "DELETE /tasks": DeleteTask,
   }
 )
 
