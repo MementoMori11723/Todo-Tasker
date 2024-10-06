d7ebf33baaa39ed13f6d0dc9e4e73c79f4383cc0
yasasvi <yasasvigumma@gmail.com>
2024-10-06 10:22:45 +0530

    used a test function
    yup the test function is running...

--- app.go
+++ app.go
@@ -11,6 +11,9 @@ func main() {
 	for route, function := range handler.Routes {
 		http.HandleFunc(route, function)
 	}
+  http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Hello, World!"))
+  })
 	log.Println("Server started on port", config.Port)
 	http.ListenAndServe(config.Port, nil)
 }
